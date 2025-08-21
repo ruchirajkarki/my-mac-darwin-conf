f64d3443d1482640671fcb560cd7801fa9473d8d
ruchirajkarki <ruchirajkarki@gmail.com>
2025-08-21 11:42:41 +0545

    fix: correct directory path in shell alias for deployment

--- home/shell.nix
+++ home/shell.nix
@@ -21,7 +21,7 @@
   home.shellAliases = {
     k = "kubectl";
     t = "turbo";
-    dd = "cd ~/nix-config && sudo make deploy";
+    dd = "cd ~/nix-conf && sudo make deploy";
 
     urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
     urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
