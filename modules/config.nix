e4a4e24a2224b07745e0066fd8bd5c3af6d4b9e2
ruchirajkarki <ruchirajkarki@icloud.com>
2025-12-09 14:45:42 +0545

    fix: update useremail from iCloud to Gmail in configuration files

--- modules/config.nix
+++ modules/config.nix
@@ -1,6 +1,6 @@
 {
   username = "ruchirajkarki";
-  useremail = "ruchirajkarki@icloud.com";
+  useremail = "ruchirajkarki@gmail.com";
   system = "aarch64-darwin"; # aarch64-darwin or x86_64-darwin
   hostname = "Ruchis-MacBook-Air";
 }
--- rich-demo/flake.nix
+++ rich-demo/flake.nix
@@ -51,7 +51,7 @@
     ...
   }: let
     username = "ruchirajkarki";
-    useremail = "ruchirajkarki@icloud.com";
+    useremail = "ruchirajkarki@gmail.com";
     system = "aarch64-darwin"; # aarch64-darwin or x86_64-darwin
     hostname = "Ruchis-MacBook-Air";
 
